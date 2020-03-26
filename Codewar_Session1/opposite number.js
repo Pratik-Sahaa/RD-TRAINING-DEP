c8b30349a452844840bd670a127e719fa687ecdc
Pratik Saha <Pratik_Saha@epam.com>
2020-03-26 17:21:16 +0530

    corrected codes

---
+++ Codewar_Session1/opposite number.js
@@ -0,0 +1,16 @@
+// Q2) Opposite Number
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// A2)
+
+function opposite(number) {
+  return number * (-1);
+}
